ace93255183aedf443ac5d615e5503cb90665ed8
Eduardo Ferrari <eduardo.ferrari@outlook.com>
2024-07-08 15:02:14 -0300

    Criado repositórios e interfaces;

---
+++ WebAPINetCoreDDD.Application/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,11 @@
+﻿using WebAPINetCoreDDD.Domain.Entities;
+
+namespace WebAPINetCoreDDD.Application.Interfaces;
+public interface ICategoriaRepository
+{
+    Task<IEnumerable<Categoria>> ListarTodosAsync();
+    Task<Categoria> BuscarPorIdAsync(int id);
+    Task AdicionarAsync(Categoria categoria);
+    Task AtualizarAsync(Categoria categoria);
+    Task DeletarAsync(int id);
+}
---
+++ WebAPINetCoreDDD.Persistence/Repositories/UsuarioRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPINetCoreDDD.Application.Interfaces;
+using WebAPINetCoreDDD.Domain.Entities;
+
+namespace WebAPINetCoreDDD.Persistence.Repositories;
+public class UsuarioRepository : IUsuarioRepository
+{
+    private readonly AppDbContext _context;
+
+    public UsuarioRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Usuario> ValidarLoginAsync(string email, string senha)
+    {
+        return await _context.Usuarios.SingleOrDefaultAsync(u => u.Email == email && u.Senha == senha);
+    }
+
+    public async Task<IEnumerable<Usuario>> ListarTodosAsync()
+    {
+        return await _context.Usuarios.ToListAsync();
+    }
+
+    public async Task<Usuario> BuscarPorIdAsync(int id)
+    {
+        return await _context.Usuarios.FindAsync(id);
+    }
+
+    public async Task AdicionarAsync(Usuario usuario)
+    {
+        await _context.Usuarios.AddAsync(usuario);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AtualizarAsync(Usuario usuario)
+    {
+        _context.Usuarios.Update(usuario);
+        await _context.SaveChangesAsync();
+    }
+
+
+    public async Task DeletarAsync(int id)
+    {
+        var user = await _context.Usuarios.FindAsync(id);
+        if (user != null)
+        {
+            _context.Usuarios.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
